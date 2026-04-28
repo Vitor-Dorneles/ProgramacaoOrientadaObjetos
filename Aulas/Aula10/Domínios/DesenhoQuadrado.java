99fab5280814e44270b330bd6b1447d5fd1882ac
Vitor-Dorneles <dorneles.vitor@ufn.edu.br>
2026-04-28 10:46:29 -0300

    Ajustes para seguir o exemplo

--- Aulas/Aula10/Domínios/DesenhoQuadrado.java
+++ Aulas/Aula10/Domínios/DesenhoQuadrado.java
@@ -1,14 +1,10 @@
 package Domínios;
 
 public class DesenhoQuadrado extends Desenho2D {
-    private int lado1;
-    private int lado2;
     protected String desc;
 
-    public DesenhoQuadrado(String nomeAutor, String descricao, int largura, int altura, int lado1, int lado2, String desc) {
+    public DesenhoQuadrado(String nomeAutor, String descricao, int largura, int altura, String desc) {
         super(nomeAutor, descricao, altura, largura);
-        this.lado1 = lado1;
-        this.lado2 = lado2;
         this.desc = descricao;
     }
 
@@ -18,8 +14,6 @@ public class DesenhoQuadrado extends Desenho2D {
         System.out.println(this.descricao);
         System.out.println(this.getLargura());
         System.out.println(getAltura());
-        System.out.println(this.lado1);
-        System.out.println(this.lado2);
         System.out.println(this.desc);
     }
     
--- Aulas/Aula10/Mains/DesenhoDados.java
+++ Aulas/Aula10/Mains/DesenhoDados.java
@@ -4,7 +4,7 @@ import Domínios.DesenhoQuadrado;
 
 public class DesenhoDados {
     public static void main(String[] args) {
-        DesenhoQuadrado desenho = new DesenhoQuadrado("VITOR", "Desenho barbada", 8, 12, 12, 12, "teste");
+        DesenhoQuadrado desenho = new DesenhoQuadrado("VITOR", "Desenho barbada", 8, 12,"opa");
         desenho.exibeDados();
     }
     
